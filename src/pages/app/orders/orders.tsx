51cced66e9dce21786676ada2b2901c13ff2d594
MDM <mdm.pro4@gmail.com>
2026-09-25 23:32:35 -0300

    feat: Add pagination component

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Pagination } from '@/components/pagination'
 import {
   Table,
   TableBody,
@@ -18,29 +19,32 @@ export function Orders() {
 
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <OrderTableFilter />
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-16"></TableHead>
-                <TableHead className="w-35">Identificador</TableHead>
-                <TableHead className="w-45">Realizado há</TableHead>
-                <TableHead className="w-35">Status</TableHead>
-                <TableHead>Clientes</TableHead>
-                <TableHead className="w-35">Total de pedidos</TableHead>
-                <TableHead className="w-41"></TableHead>
-                <TableHead className="w-33"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {Array.from({ length: 7 }).map((_, i) => {
-                return <OrderTableRow key={i} />
-              })}
-            </TableBody>
-          </Table>
+
+        <div className="space-y-2.5">
+          <OrderTableFilter />
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-16"></TableHead>
+                  <TableHead className="w-35">Identificador</TableHead>
+                  <TableHead className="w-45">Realizado há</TableHead>
+                  <TableHead className="w-35">Status</TableHead>
+                  <TableHead>Clientes</TableHead>
+                  <TableHead className="w-35">Total de pedidos</TableHead>
+                  <TableHead className="w-41"></TableHead>
+                  <TableHead className="w-33"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 7 }).map((_, i) => {
+                  return <OrderTableRow key={i} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
